9a16a3d2b87eb78df6e719878ea71641cd3166c5
Halcyonix <halcyonix@hotmail.com>
2014-12-04 09:13:57 -0800

    Create project13.ino

---
+++ Project-13---Touchy-Feely-Lamp/project13.ino
@@ -0,0 +1,26 @@
+//import CapacitiveSensor library
+#include <CapacitiveSensor.h>
+CapacitiveSensor capSensor = CapacitiveSensor(4, 2);
+
+//sets up the threshold
+int threshold = 1000
+const int ledPin = 12;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+  //Senses touch on the metal foil
+void loop() {
+  long sensorValue = capSensor.capacitiveSensor(30);
+  Serial.println(sensorValue);
+  
+  //controls if the LED is on or off
+  if(sensorValue > threshold){
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+  delay (10);
+}
